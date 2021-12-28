20c7901cd962e7d9afb1670c3be42044e960249e
p_penkovskiy <p.penkovskiy@gmail.com>
2021-12-28 23:20:02 +0300

    commit#7

--- generator/views.py
+++ generator/views.py
@@ -10,7 +10,10 @@ def password(request):
     characters = list('abcdefghijklmnopqrstuvwxyz')
     if request.GET.get('uppercase'):
         characters.extend(list('abcdefghijklmnopqrstuvwxyz'.upper()))
-
+    if request.GET.get('special'):
+        characters.extend(list("!£@#$%^&*()"))
+    if request.GET.get('numbers'):
+        characters.extend(list("1234567890"))
     length = int(request.GET.get('length', 12))
     thepassword = ''
     for x in range(length):
