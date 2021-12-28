2a4b21e033a83b422c62b90725eec985546d01eb
p_penkovskiy <p.penkovskiy@gmail.com>
2021-12-28 22:54:26 +0300

    commit#5

--- generator/views.py
+++ generator/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+import random
 
 # Create your views here.
 
@@ -6,5 +7,9 @@ def home(request):
     return render(request, 'generator/home.html')
 
 def password(request):
-    thepassword = 'testing'
+    characters = list('abcdefghijklmnopqrstuvwxyz')
+    length = 10
+    thepassword = ''
+    for x in range(length):
+        thepassword += random.choice(characters)
     return render(request, 'generator/password.html', {'password':thepassword})
