0e0a9eab3d312da8e9e92e4dc959793ee325e32c
p_penkovskiy <p.penkovskiy@gmail.com>
2021-12-29 15:03:06 +0300

    commit#8

--- generator/views.py
+++ generator/views.py
@@ -19,3 +19,6 @@ def password(request):
     for x in range(length):
         thepassword += random.choice(characters)
     return render(request, 'generator/password.html', {'password':thepassword})
+
+def tutorial(request):
+    return render(request, 'generator/about.html')
