5a12bf409ab110532081fc6a0a9617eb4cc44ff7
p_penkovskiy <p.penkovskiy@gmail.com>
2022-01-08 12:44:00 +0300

    commit#12

--- password_generator/settings.py
+++ password_generator/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-4^$$+$-erafo)=istm#tc3hpwxyp)x0o70)tn#xbb$@#j^x5(a'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
